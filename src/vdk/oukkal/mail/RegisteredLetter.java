541fce4b21f6c96217c7a6b61fba42392a0c313c
mohamedAminO <niwa4@hotmail.fr>
2014-11-12 10:41:47 +0100

    modifier RegisteredLetter

--- src/vdk/oukkal/mail/RegisteredLetter.java
+++ src/vdk/oukkal/mail/RegisteredLetter.java
@@ -6,25 +6,23 @@ public class RegisteredLetter extends Letter<Letter<?>> {
 	public RegisteredLetter(Inhabitant sender, Inhabitant receiver,
 			Letter<?> content) {
 		super(sender, receiver, content);
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return "This is a RegisteredLetter";
 	}
 
-	@Override
+	
 	public float getCost() {
-		// TODO Auto-generated method stub
 		return REGISTEREDCOST+content.getCost();
 	}
 
-	@Override
+	
 	public void action() {
-		// TODO Auto-generated method stub
-		
+		this.content.action();
+		this.receiver.getCity().sendLetter(new SimpleLetter(this.receiver,this.sender,new TextContent("Acknowledgment of receipt")));
+
 	}
 
 }
