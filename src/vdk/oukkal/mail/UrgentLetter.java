3a0d0f9159571ffce1609c0bc704007fbc48859d
mohamedAminO <niwa4@hotmail.fr>
2014-11-12 10:50:55 +0100

    Create UrgentLetter

---
+++ src/vdk/oukkal/mail/UrgentLetter.java
@@ -0,0 +1,26 @@
+package vdk.oukkal.mail;
+
+public class UrgentLetter extends Letter<Letter<?>> {
+
+	public UrgentLetter(Inhabitant sender, Inhabitant receiver,
+			Letter<?> content) {
+		super(sender, receiver, content);
+	}
+
+	@Override
+	public String getDescription() {
+		return "This is a UrgentLetter";
+	}
+
+	@Override
+	public float getCost() {
+		return 2*this.content.getCost();
+	}
+
+	@Override
+	public void action() {
+		this.content.action();
+
+	}
+
+}
