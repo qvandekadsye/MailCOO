407e93c4db13d984a65f3fa9c49caee67542a630
mohamedAminO <niwa4@hotmail.fr>
2014-11-19 11:02:51 +0100

    Main changed

--- src/vdk/oukkal/mail/Main.java
+++ src/vdk/oukkal/mail/Main.java
@@ -6,6 +6,21 @@ package vdk.oukkal.mail;
  */
 public class Main {
 
+	private int nbI;
+	private int nbDay;
+
+
+	public Main(int nbI, int nbDay){
+		this.nbI = nbI;
+		this.nbDay = nbDay;
+	}
+
+
+	public Main(){
+		this.nbI = 100;
+		this.nbDay = 6;
+	}
+
 
 	public Letter<?> createLetter(Inhabitant sender, Inhabitant receiver, int i){
 		if(i == 0){
@@ -19,53 +34,54 @@ public class Main {
 			int j = (int)(Math.random() * ((4 - 1) + 1));
 			return new RegisteredLetter(sender, receiver, createLetter(sender, receiver,j));
 		}
-
 		int j = (int)(Math.random() * ((4 - 1) + 1));
 		return new UrgentLetter(sender, receiver, createLetter(sender, receiver,j));
-
-
 	}
+
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if(args.length == 1)
+			Main main = new Main();
+		else if (args.length = 3)
+			Main main = new Main(args[1],args[2]);
+			City city = new City("Lille");
+			System.out.println("Creating "+city.getName()+" city"); 
 
-		Main main = new Main();
-		City city = new City("Lille");
-		System.out.println("Creating "+city.getName()+" city"); 
+			int nbI = Integer.parseInt(args[0]) ; 
 
-		int nbI = 100 ; 
-
-		for(int i = 0 ; i < nbI ; i++){
-			String name = "Inhabitant-"+i;
-			city.addInhabitant(new Inhabitant(city, name , new BankAccount("bc"+i,10)));
-		}
-
-		System.out.println("Creating "+nbI+" inhabitants");
-
-
-		int k = 6;
-
-		System.out.println("Mailing letters for "+k+" days");
-
-		for(int j = 0 ; j < k; j++){
-			
-			System.out.println("************************************************************");
-			System.out.println("Day "+j);
-			if(j!=0)
-				city.distributeLetters();
-			for(int n = 0 ; n < 2 ;n++){
-				int aleaSender = (int)(Math.random() * ((99 - 1) + 1));
-				int aleaReceiver = (int)(Math.random() * ((99 - 1) + 1));
-				int typeLetter = (int)(Math.random() * ((4 - 1) + 1));
-				Letter<?> l = main.createLetter(city.getInhabitants().get(aleaSender), city.getInhabitants().get(aleaReceiver), typeLetter);
-				city.sendLetter(l);
+			for(int i = 0 ; i < nbI ; i++){
+				String name = "Inhabitant-"+i;
+				city.addInhabitant(new Inhabitant(city, name , new BankAccount("bc"+i,10)));
 			}
 
+			System.out.println("Creating "+nbI+" inhabitants");
 
+
+			int k = Integer.parseInt(args[1]) ; 
+
+			System.out.println("Mailing letters for "+k+" days");
+
+			for(int j = 0 ; j < k; j++){
+
+				System.out.println("************************************************************");
+				System.out.println("Day "+j);
+				if(j!=0)
+					city.distributeLetters();
+				for(int n = 0 ; n < 2 ;n++){
+					int aleaSender = (int)(Math.random() * ((99 - 1) + 1));
+					int aleaReceiver = (int)(Math.random() * ((99 - 1) + 1));
+					int typeLetter = (int)(Math.random() * ((4 - 1) + 1));
+					Letter<?> l = main.createLetter(city.getInhabitants().get(aleaSender), city.getInhabitants().get(aleaReceiver), typeLetter);
+					city.sendLetter(l);
+				}
+
+
+			}
 		}
-
 
 
 
